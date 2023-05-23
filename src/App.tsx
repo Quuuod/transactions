62af1ac7e7ba48e65732d059ff5a220476f48a03
Quuuod <prizraklogan@gmail.com>
2023-05-23 11:41:32 +0300

    created test project using react+typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex m-auto items-center justify-center p-10 bg-slate-300">
+      <h1 className="">Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
